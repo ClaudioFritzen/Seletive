6d4434eda3f06c3dd10cfd6f0e1623086e76f067
ClaudioFritzen <sdfritzen96@gmail.com>
2022-11-09 17:44:30 +0000

    criamos as table iniciais

--- empresa/models.py
+++ empresa/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Tecnologias(models.Model):
+    tecnologia = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tecnologia
+
 class Empresa(models.Model):
 
     choices_nicho_mercado = (
@@ -10,9 +16,15 @@ class Empresa(models.Model):
         ('D', 'Design'),
     )
 
+    logo = models.ImageField(upload_to="logo_empresa", null=True)
     nome = models.CharField(max_length=30)
     email = models.EmailField()
     cidade = models.CharField(max_length=30)
     endereco = models.CharField(max_length=30)
-    caracterista_empresa = models.TextField()
+    caracteristica_empresa = models.TextField()
     nicho_mercado = models.CharField(max_length=3, choices = choices_nicho_mercado)
+
+    tecnologias = models.ManyToManyField(Tecnologias)
+
+    def __str__(self):
+        return self.nome
