69338d5b84c07ab6f11991f8a902dcd695f18c18
ClaudioFritzen <sdfritzen96@gmail.com>
2022-11-15 16:07:46 +0000

    criamos o css

--- seletive/urls.py
+++ seletive/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# para pegar as img que o usuario coloca no banco
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('empresa.urls')),
