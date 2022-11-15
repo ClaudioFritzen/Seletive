10339e7b3a993bfed98ddbdf057cd250878f0604
ClaudioFritzen <sdfritzen96@gmail.com>
2022-11-15 17:12:15 +0000

    deixando o form de nova empresa dinamico

--- seletive/urls.py
+++ seletive/urls.py
@@ -3,8 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 # para pegar as img que o usuario coloca no banco
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', include('empresa.urls')),
+
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
